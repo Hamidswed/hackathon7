9bbc978095532193e9b325e2c92374cb3bcdbbe7
HamidReza <delshad.swdn@gmail.com>
2023-01-30 16:22:26 +0100

    add slide menu

---
+++ src/components/slide/Slide.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Divider } from "@mui/material";
+import CodeIcon from "@mui/icons-material/Code";
+import ApiIcon from '@mui/icons-material/Api';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import SettingsIcon from '@mui/icons-material/Settings';
+import RouteIcon from '@mui/icons-material/Route';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+export default function Slide() {
+  const [devOpen, setDevOpen] = React.useState(false);
+  const [planOpen, setPlanOpen] = React.useState(false);
+
+  const handleClickDev = () => {
+    setDevOpen(!devOpen);
+  };
+  const handleClickPlan = () => {
+    setPlanOpen(!planOpen);
+  };
+
+  return (
+    <List
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "#e5e5e5", paddingTop:"30px", height:"100vh" }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      // subheader={
+      //   <ListSubheader component="div" id="nested-list-subheader">
+      //     Project name
+      //   </ListSubheader>
+      // }
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText primary="Project Name" />
+      </ListItemButton>
+      <ListItemButton onClick={handleClickPlan}>
+        <ListItemIcon>
+          <AppRegistrationIcon />
+        </ListItemIcon>
+        <ListItemText primary="PLANING" />
+        {planOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={planOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <RouteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Road map" />
+          </ListItemButton>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Board" />
+          </ListItemButton>
+        </List>
+      </Collapse>
+      <ListItemButton onClick={handleClickDev}>
+        <ListItemIcon>
+          <ApiIcon />
+        </ListItemIcon>
+        <ListItemText primary="DEVELOPMENT" />
+        {devOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={devOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <CodeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Code" />
+          </ListItemButton>
+        </List>
+      </Collapse>
+      <Divider sx={{margin:"10px 0"}} />
+      <ListItemButton>
+        <ListItemIcon>
+          <DocumentScannerIcon />
+        </ListItemIcon>
+        <ListItemText primary="Project pages" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <LibraryAddIcon />
+        </ListItemIcon>
+        <ListItemText primary="Add shortcut" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <SettingsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Project settings" />
+      </ListItemButton>
+    </List>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Slider from "./../components/slide/Slide";
+
+const Home = () => {
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+};
+export default Home;
