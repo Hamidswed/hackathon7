10d6cf2cb85ca74ff7f37475482e4ed5a87dc448
User <mtgnatali@gmail.com>
2023-01-31 17:34:25 +0200

    add styles for icons

--- src/Components/singleTask/SingleTask.tsx
+++ src/Components/singleTask/SingleTask.tsx
@@ -20,17 +20,17 @@ const SingleTask = ({name,onDelete,onToggleDone, onToggleImportant, addNewTask, 
   return (
     <div className='task-container'>
       <li className="task-item">
-            <p className='status' onClick={onToggleDone}>{name}</p>
+            <p className={`task-subtitle ${important ? 'task-subtitle__important' : ''} ${done ? 'task-subtitle__done' : ''}`} onClick={onToggleDone}>{name}</p>
             <div className="icon-container">
                 <button type="button" className="btn btn-outline-success" onClick={onToggleImportant}>
                     <ImportantIcon/>
                 </button>
 
-                <button type="button" className="btn btn-outline-primary ms-1" onClick={onToggleDone}>
+                <button type="button" className="btn btn-outline-primary" onClick={onToggleDone}>
                     <DoneIcon/>
                 </button>
 
-                <button type="button" className="btn btn-outline-danger ms-1" onClick={onDelete}>
+                <button type="button" className="btn btn-outline-danger" onClick={onDelete}>
                     <DeleteIcon/>
                 </button>
                 
